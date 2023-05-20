d838fa4aeca77b7ff9614a5d885402024025a017
LucasCto <1lucascastillox123@gmail.com>
2023-05-19 21:36:42 -0300

    Added: Salir and Resta

--- calculadoraEjer.py
+++ calculadoraEjer.py
@@ -1,9 +1,17 @@
 while True:
     print("-----Calculadora-----")
     print("     a-Sumar")
-    print("     b-Salir")
+    print("     b-Resta")
+    print("     c-Salir")
     menu = input("Sleccione la opcion: ")
     if menu == "a":
         x = int(input("ingrese el primer numero "))
         y = int(input("ingrese el segundo numero "))
         print(f"La suma de ", x, " y ", y, "es ", x+y) 
+    elif menu == "b":
+        x = int(input("ingrese el primer numero "))
+        y = int(input("ingrese el segundo numero "))
+        print(f"La resta de ", x, " menos ", y, "es ", x-y) 
+    elif menu == "c";
+        print("Hasta Luego!")
+        break
