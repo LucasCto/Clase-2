c1da9f1b8c7e6b22ea46932226116ddaeaf5ea7f
LucasCto <1lucascastillox123@gmail.com>
2023-05-19 21:47:22 -0300

    fix: se removio la "f" de los print

--- calculadoraEjer.py
+++ calculadoraEjer.py
@@ -3,20 +3,25 @@ while True:
     print("     a-Sumar")
     print("     b-Resta")
     print("     c-Multiplicar")
-    print("     d-Salir")
+    print("     d-Division")
+    print("     e-Salir")
     menu = input("Sleccione la opcion: ")
     if menu == "a":
         x = int(input("ingrese el primer numero "))
         y = int(input("ingrese el segundo numero "))
-        print(f"La suma de ", x, " y ", y, "es ", x+y) 
+        print("La suma de ", x, " y ", y, "es ", x+y) 
     elif menu == "b":
         x = int(input("ingrese el primer numero "))
         y = int(input("ingrese el segundo numero "))
-        print(f"La resta de ", x, " menos ", y, "es ", x-y) 
+        print("La resta de ", x, " menos ", y, "es ", x-y) 
     elif menu == "c":
         x = int(input("ingrese el primer numero "))
         y = int(input("ingrese el segundo numero "))
-        print(f"La multiplicacion de ", x, " por ", y, "es ", x*y) 
+        print("La multiplicacion de ", x, " por ", y, "es ", x*y) 
     elif menu == "d":
+        x = int(input("ingrese el primer numero "))
+        y = int(input("ingrese el segundo numero "))
+        print("La division de ", x, " sobre ", y, "es ", x/y) 
+    elif menu == "e":
         print("Hasta Luego!")
         break
