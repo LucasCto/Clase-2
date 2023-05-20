8c1ab70ba85dde1df2b5de413311e5cdf3fe401f
LucasCto <1lucascastillox123@gmail.com>
2023-05-19 21:39:22 -0300

    Added: Multiplicar

--- calculadoraEjer.py
+++ calculadoraEjer.py
@@ -2,7 +2,8 @@ while True:
     print("-----Calculadora-----")
     print("     a-Sumar")
     print("     b-Resta")
-    print("     c-Salir")
+    print("     c-Multiplicar")
+    print("     d-Salir")
     menu = input("Sleccione la opcion: ")
     if menu == "a":
         x = int(input("ingrese el primer numero "))
@@ -12,6 +13,10 @@ while True:
         x = int(input("ingrese el primer numero "))
         y = int(input("ingrese el segundo numero "))
         print(f"La resta de ", x, " menos ", y, "es ", x-y) 
-    elif menu == "c";
+    elif menu == "c":
+        x = int(input("ingrese el primer numero "))
+        y = int(input("ingrese el segundo numero "))
+        print(f"La multiplicacion de ", x, " por ", y, "es ", x*y) 
+    elif menu == "d":
         print("Hasta Luego!")
         break
